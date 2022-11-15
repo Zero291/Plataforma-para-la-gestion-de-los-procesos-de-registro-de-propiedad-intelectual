0f10207902c72b7cec2b71183dbf3512382cd96d
Luis Flores <117026218+LuisRFE0@users.noreply.github.com>
2022-11-15 10:36:44 -0600

    Add files via upload
    script de validacion de formulario de registro de cuenta

---
+++ js/validacionCuenta.js
@@ -0,0 +1,103 @@
+// variable de objetos
+
+const datos = {
+    nombre: "",
+    apellidop: "",
+    apellidom: "",
+    tecnologico: "",
+    telefono: "",
+    correo: "",
+    contraseña: "",
+    contraseñac: ""
+};
+
+const nombre = document.querySelector("#nombre");
+const apellidop = document.querySelector("#apellidop");
+const apellidom = document.querySelector("#apellidom");
+const tecnologico = document.querySelector("#tecnologico");
+const telefono = document.querySelector("#telefono");
+const correo = document.querySelector("#correo");
+const contraseña = document.querySelector("#passC");
+const contraseñac = document.querySelector("#passCC");
+
+
+nombre.addEventListener("input", leerTexto);
+apellidop.addEventListener("input", leerTexto);
+apellidom.addEventListener("input", leerTexto);
+tecnologico.addEventListener("input", leerTexto);
+telefono.addEventListener("input", leerTexto);
+correo.addEventListener("input", leerTexto);
+contraseña.addEventListener("input", leerTexto);
+contraseñac.addEventListener("input", leerTexto);
+
+
+
+const formulario = document.querySelector("#formulario");
+formulario.addEventListener("submit", function (e) {
+    const { nombre, apellidop, apellidom, tecnologico, telefono, correo, contraseña, contraseñac } = datos;
+
+e.preventDefault();
+    if (nombre === "" || apellidop === "" || apellidom === "" || tecnologico==="" || telefono==="" || correo==="") {
+       
+        mostrarError("Por Favor Llene Todos Los Campos");
+
+        setTimeout(() => {
+            location.reload();
+        }, 5000);
+
+        return;
+    } else {
+
+        mostrarEnviar("Datos Enviados Correctamente")
+
+    }
+
+
+});
+
+
+
+//leer la informacion introducida en los campos
+function leerTexto(e) {
+    datos[e.target.id] = e.target.value;
+}
+
+
+
+
+//funcion para mostrar error en pantalla
+
+function mostrarError(mensaje){
+    const error=document.createElement("P");
+    error.textContent=mensaje;
+    error.classList.add("error");
+    formulario.appendChild(error);
+    setTimeout(() => {
+        error.remove();
+    }, 5000);
+
+
+
+
+
+
+}
+
+function mostrarEnviar(mensaje){
+    const enviar=document.createElement("P");
+    enviar.textContent=mensaje;
+    enviar.classList.add("enviar");
+    formulario.appendChild(enviar);
+    setTimeout(() => {
+        error.remove();
+    }, 5000);s
+
+
+
+
+
+
+}
+
+
+
